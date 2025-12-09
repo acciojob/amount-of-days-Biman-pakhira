04903395790456eeb462bf8343f42d9f3712c934
Biman Pakhira <bimanpakhira2@gmail.com>
2025-12-10 00:08:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function leapCal(year){
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+		return 366;
+	} else {
+		return 365;
+	}
+}
+
+let year = prompt("Enter year");
+alert(leapCal(year));
